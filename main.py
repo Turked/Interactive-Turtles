12b3d84d4e089a4c34d9333a3bd3ed332faba5b7
Edward Turk <91563267+Turked@users.noreply.github.com>
2021-10-19 19:21:01 +0000

    Started on the new walls 10/19/21

--- main.py
+++ main.py
@@ -25,11 +25,52 @@ window.setup(screen_width, screen_height)
 #Screen:
 
 
-#List setup:
-wall_list = []
-w1 = Wall(256, 190, 4, 4)
-wall_list.append(w1)
 
+
+#Mega Walls:________________________________________
+
+#Setup:
+wall_list = []
+
+
+#Vairaibles:
+starting_wall = Wall(-280, 180, 1, 5)
+last_x = starting_wall.xcor()
+last_y = starting_wall.ycor()
+last_width = starting_wall.x_size
+last_height = starting_wall.y_size
+
+callibration = [-10, 1, 10]
+
+#Creation of the first wall:
+wall_list.append(starting_wall)
+
+#Loop for amount of walls:
+for i in range(5):
+
+# Horrizontal Wall
+  my_width = randint(3, 6)
+  my_height = 1
+  last_x = last_x + (my_width * 10) - 10
+  last_y = last_y - (last_height) * 10
+  last_width = my_width
+  last_height = my_height
+  wall_list.append(Wall(last_x, last_y, my_width, my_height))
+
+# Vertical Wall
+  my_width = 1
+  my_height = randint(3, 6)
+  last_x = last_x + (last_width * 10) - 10
+  last_y = last_y - (my_height) * 10
+  last_width = my_width
+  last_height = my_height
+  wall_list.append(Wall(last_x, last_y, my_width, my_height))
+
+#Score Board:
+wall_list.append(Wall(256, 190, 4, 4))
+
+
+'''
 #Vertical walls:
 color = randcolorwall() #Maroon shouldn't exist as a color but it does
 for wall in range (12):
@@ -42,6 +83,7 @@ for wall in range (12):
   wall_list[len(wall_list)-1].color(color)
 
 wall_list.append(Wall(256, 190, 4, 4))
+'''
 
 #Text
 print("Here's the deal.")
